db72e439bccd89107fa5adae29604c1773121c64
Robert Haines <rhaines@manchester.ac.uk>
2022-08-21 15:18:48 +0100

    Begin implementing InputStream.

---
+++ test/rubyzip/fixtures.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+# Fixtures directories.
+FIXTURES_DIR = File.expand_path('fixtures', __dir__)
+ZIP_FIXTURES = File.join(FIXTURES_DIR, 'zip')
+
+# Test zips.
+ZIP_ONE_TEXT_FILE = File.join(ZIP_FIXTURES, 'one_text_file.zip')
