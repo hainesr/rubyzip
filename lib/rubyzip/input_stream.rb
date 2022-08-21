5b7423e6d2a73a13b271ecea07031ea3f69daf2e
Robert Haines <rhaines@manchester.ac.uk>
2022-08-21 18:22:41 +0100

    Add InputStream#next_entry.

--- lib/rubyzip/input_stream.rb
+++ lib/rubyzip/input_stream.rb
@@ -4,6 +4,9 @@
 #
 # Licensed under the BSD License. See LICENCE for details.
 
+require_relative 'entry'
+require_relative 'utilities'
+
 ##
 module Rubyzip
   # InputStream provides a simple streaming interface for reading zip entries
@@ -12,6 +15,12 @@ module Rubyzip
     def initialize(io)
       @io = io
       @current_entry = nil
+    end
+
+    def next_entry
+      name, = Utilities.read_local_header(@io)
+
+      Entry.new(name)
     end
   end
 end
