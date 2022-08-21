1ddcb6222725067705851b7d638ffd97afd9c59c
Robert Haines <rhaines@manchester.ac.uk>
2022-08-21 18:12:11 +0100

    Add a top-level Rubyzip Error class.

---
+++ lib/rubyzip/errors.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+##
+module Rubyzip
+  # The general error raised by Rubyzip and the superclass of other
+  # Rubyzip errors.
+  class Error < StandardError; end
+end
