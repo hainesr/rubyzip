577dbe5a2aa5a12fbe4b76e059a95724ece11035
Robert Haines <rhaines@manchester.ac.uk>
2022-08-21 23:18:00 +0100

    Add zip fixtures: a stored text file and a directory.

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -11,6 +11,8 @@ ZIP_FIXTURES = ::File.join(FIXTURES_DIR, 'zip')
 
 # Test zipz.
 ZIP_ONE_TEXT_FILE = ::File.join(ZIP_FIXTURES, 'one_text_file.zip')
+ZIP_ONE_TEXT_FILE_STOR = ::File.join(ZIP_FIXTURES, 'one_text_file_stored.zip')
+ZIP_ONE_DIRECTORY = ::File.join(ZIP_FIXTURES, 'one_directory.zip')
 
 # Test binary data.
 BIN_LOCAL_HEADER = ::File.join(BIN_FIXTURES, 'local_header.bin')
