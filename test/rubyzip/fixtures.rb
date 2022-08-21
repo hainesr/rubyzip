5fb1e147355ed9facec4ad00cef836f56a0c2815
Robert Haines <rhaines@manchester.ac.uk>
2022-08-21 16:44:19 +0100

    Add a utility to read a local header.
    This returns the name and the header data and extra field data as three
    separate strings.

--- test/rubyzip/fixtures.rb
+++ test/rubyzip/fixtures.rb
@@ -6,7 +6,11 @@
 
 # Fixtures directories.
 FIXTURES_DIR = File.expand_path('fixtures', __dir__)
+BIN_FIXTURES = File.join(FIXTURES_DIR, 'bin')
 ZIP_FIXTURES = File.join(FIXTURES_DIR, 'zip')
 
 # Test zips.
 ZIP_ONE_TEXT_FILE = File.join(ZIP_FIXTURES, 'one_text_file.zip')
+
+# Test binary data.
+BIN_LOCAL_HEADER = File.join(BIN_FIXTURES, 'local_header.bin')
