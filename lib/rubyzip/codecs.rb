f99b232a414604b0769a0b6ac6ebbd1f5abf547a
Robert Haines <rhaines@manchester.ac.uk>
2022-08-23 18:04:01 +0100

    Add a Codec registry for easy lookup.
    Just has decompressors for now, but easily extended.

---
+++ lib/rubyzip/codecs.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2022, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+##
+module Rubyzip
+  # A registry of compression and decompression methods.
+  module Codecs
+    @decompressors = {}
+
+    def self.register_decompressor(clazz)
+      return unless clazz.const_defined?(:DECOMPRESSION_METHOD)
+
+      @decompressors[clazz.const_get(:DECOMPRESSION_METHOD)] = clazz
+    end
+
+    def self.decompressor_for(compression_method)
+      @decompressors[compression_method]
+    end
+
+    def self.decompressor_for_entry(entry)
+      decompressor_for(entry.compression_method)
+    end
+  end
+end
---
+++ test/codecs_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2022, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative 'test_helper'
+
+require 'rubyzip/codecs'
+
+class CodecsTest < MiniTest::Test
+  TEST_METHOD_NOT_STORED = -99
+  TEST_DECOMPRESSION_METHOD = -1
+
+  # Fake decompressor.
+  class TestDecompressor
+    DECOMPRESSION_METHOD = TEST_DECOMPRESSION_METHOD
+  end
+
+  # Fake entry which uses fake decompressor.
+  class TestEntry
+    attr_reader :compression_method
+
+    def initialize
+      @compression_method = TEST_DECOMPRESSION_METHOD
+    end
+  end
+
+  def setup
+    Rubyzip::Codecs.register_decompressor(TestDecompressor)
+  end
+
+  def test_register_non_codecs
+    Rubyzip::Codecs.register_decompressor(String)
+    refute(Rubyzip::Codecs.instance_variable_get(:@decompressors).value?(String))
+  end
+
+  def test_decompressor_for
+    assert_equal(
+      TestDecompressor,
+      Rubyzip::Codecs.decompressor_for(TEST_DECOMPRESSION_METHOD)
+    )
+
+    assert_nil(Rubyzip::Codecs.decompressor_for(TEST_METHOD_NOT_STORED))
+  end
+
+  def test_decompressor_for_entry
+    entry = TestEntry.new
+
+    assert_equal(
+      TestDecompressor,
+      Rubyzip::Codecs.decompressor_for_entry(entry)
+    )
+  end
+end
