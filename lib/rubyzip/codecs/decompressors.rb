5652b4360c5a549078cb391740fc6afb5245cafa
Robert Haines <rhaines@manchester.ac.uk>
2022-08-23 21:20:18 +0100

    Auto-load and register decompressors.

---
+++ lib/rubyzip/codecs/decompressors.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative 'inflater'
+require_relative 'stored_decompressor'
