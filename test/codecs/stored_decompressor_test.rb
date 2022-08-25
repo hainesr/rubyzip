82b659c79763d739e49dc85521179aa0233243ec
Robert Haines <rhaines@manchester.ac.uk>
2022-08-25 22:05:06 +0100

    Add isolated tests for StoredDecompressor.

---
+++ test/codecs/stored_decompressor_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2022, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative '../test_helper'
+
+require 'rubyzip/codecs/stored_decompressor'
+
+class StoredDecompressorTest < MiniTest::Test
+  # Fake entry which implements uncompressed_size.
+  class TestEntry
+    attr_reader :uncompressed_size
+
+    def initialize(size)
+      @uncompressed_size = size
+    end
+  end
+
+  def test_read
+    text = ::File.read(TXT_LOREM_IPSUM)
+
+    ::File.open(TXT_LOREM_IPSUM, 'rb') do |is|
+      entry = TestEntry.new(::File.size(TXT_LOREM_IPSUM))
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+
+      assert_equal(text, decompressor.read)
+    end
+  end
+
+  def test_read_read_partial_lengths
+    text = ::File.read(TXT_LOREM_IPSUM)
+    half_len = text.length / 2
+
+    ::File.open(TXT_LOREM_IPSUM, 'rb') do |is|
+      entry = TestEntry.new(::File.size(TXT_LOREM_IPSUM))
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+
+      assert_empty(decompressor.read(0))
+      assert_equal(text[(0...half_len)], decompressor.read(half_len))
+      assert_equal(text[(half_len...text.length)], decompressor.read)
+    end
+  end
+
+  def test_read_at_eof
+    ::File.open(TXT_LOREM_IPSUM, 'rb') do |is|
+      entry = TestEntry.new(::File.size(TXT_LOREM_IPSUM))
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+      decompressor.read
+
+      assert_empty(decompressor.read(0))
+      assert_empty(decompressor.read)
+      assert_nil(decompressor.read(1))
+    end
+  end
+end
