25ebd982ca13f6c60b1d4b1320e180ab6506117b
Robert Haines <rhaines@manchester.ac.uk>
2022-08-26 15:44:13 +0100

    Add a test for reading multiple entries from an InputStream.

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -15,6 +15,7 @@ ZIP_ONE_TEXT_FILE = ::File.join(ZIP_FIXTURES, 'one_text_file.zip')
 ZIP_ONE_TEXT_FILE_STOR = ::File.join(ZIP_FIXTURES, 'one_text_file_stored.zip')
 ZIP_ONE_DIRECTORY = ::File.join(ZIP_FIXTURES, 'one_directory.zip')
 ZIP_ONE_PNG_FILE = ::File.join(ZIP_FIXTURES, 'one_png_file.zip')
+ZIP_MULTI_FILE = ::File.join(ZIP_FIXTURES, 'multi_file.zip')
 
 # Test binary data.
 BIN_LOCAL_HEADER = ::File.join(BIN_FIXTURES, 'local_header.bin')
