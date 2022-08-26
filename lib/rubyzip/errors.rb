be485e256944b410bafa8d0c17d3cd66dfddb11b
Robert Haines <rhaines@manchester.ac.uk>
2022-08-26 23:03:30 +0100

    Validate CRC-32 checksums after decompression of an entry.

--- lib/rubyzip/errors.rb
+++ lib/rubyzip/errors.rb
@@ -9,4 +9,20 @@ module Rubyzip
   # The general error raised by Rubyzip and the superclass of other
   # Rubyzip errors.
   class Error < StandardError; end
+
+  # This error is raised if there is a mismatch between CRC-32 checksums
+  # when decompressing an entry.
+  class CRC32Error < Error
+    def initialize(expected, actual)
+      super()
+
+      @expected = expected
+      @actual = actual
+    end
+
+    def message
+      'Invalid CRC32 checksum when decompressed - ' /
+        "expected 0x#{@expected}; got 0x#{@actual}."
+    end
+  end
 end
