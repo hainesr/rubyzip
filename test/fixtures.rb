17c550c040e0e54c834ead861accdada4a7e920a
Robert Haines <rhaines@manchester.ac.uk>
2022-08-26 14:16:03 +0100

    Add a test for reading a binary entry from an InputStream.

--- test/fixtures.rb
+++ test/fixtures.rb
@@ -14,10 +14,12 @@ ZIP_FIXTURES = ::File.join(FIXTURES_DIR, 'zip')
 ZIP_ONE_TEXT_FILE = ::File.join(ZIP_FIXTURES, 'one_text_file.zip')
 ZIP_ONE_TEXT_FILE_STOR = ::File.join(ZIP_FIXTURES, 'one_text_file_stored.zip')
 ZIP_ONE_DIRECTORY = ::File.join(ZIP_FIXTURES, 'one_directory.zip')
+ZIP_ONE_PNG_FILE = ::File.join(ZIP_FIXTURES, 'one_png_file.zip')
 
 # Test binary data.
 BIN_LOCAL_HEADER = ::File.join(BIN_FIXTURES, 'local_header.bin')
 BIN_LOREM_IPSUM_DEFLATED = ::File.join(BIN_FIXTURES, 'lorem_ipsum_deflated.bin')
+BIN_PNG_FILE = ::File.join(BIN_FIXTURES, 'zip.png')
 
 # Test text data.
 TXT_LOREM_IPSUM = ::File.join(TXT_FIXTURES, 'lorem_ipsum.txt')
