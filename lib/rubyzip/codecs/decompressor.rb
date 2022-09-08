e99f3edfe146b627a7cf0d555be20c82ea456585
Robert Haines <rhaines@manchester.ac.uk>
2022-09-08 23:53:05 +0100

    Wire decryption into the InputStream pipeline.
    This required a wholesale refactor of how streams were connected to each
    other. EntryInputStream is now used to connect a decryption stream to a
    decompression stream. This also (correctly) moves 'Entry level' checks
    (such as CRC32) to the EntryInputStream - this makes sense as CRC32 only
    exists in the context of an Entry; not the decompression stream.

--- lib/rubyzip/codecs/decompressor.rb
+++ lib/rubyzip/codecs/decompressor.rb
@@ -4,8 +4,6 @@
 #
 # Licensed under the BSD License. See LICENCE for details.
 
-require 'zlib'
-
 require_relative '../constants'
 
 ##
@@ -13,41 +11,17 @@ module Rubyzip
   module Codecs
     # This is the superclass for all decompressors.
     #
-    # It calculates a CRC-32 checksum as it decompresses, which can
-    # be validated.
-    #
     # Subclasses are required to implement `read_stream` and `eof?`.
     class Decompressor
-      def initialize(io, entry)
+      def initialize(io, data_length)
         @io = io
-        @entry = entry
-        @remaining_data = @entry.compressed_size
-        @processed_data = 0
-        @crc32 = Zlib.crc32
-        @output_data_length_warning = false
+        @remaining_data = data_length
       end
 
       def read(len = nil)
         return (len.nil? || len.zero? ? '' : nil) if eof?
 
-        buf = read_stream(len)
-        @processed_data += buf.length
-        if !@output_data_length_warning && (@processed_data > @entry.uncompressed_size)
-          @output_data_length_warning = true
-          warn "Entry '#{@entry.name}' should be #{@entry.uncompressed_size}B, " \
-               'but is larger when inflated.'
-        end
-        @crc32 = Zlib.crc32(buf, @crc32)
-
-        buf
-      end
-
-      def valid_crc32?
-        @entry.crc32 == @crc32
-      end
-
-      def validate_crc32!
-        raise CRC32Error.new(@entry.crc32, @crc32) unless valid_crc32?
+        read_stream(len)
       end
     end
   end
--- test/rubyzip/codecs/stored_decompressor_test.rb
+++ test/rubyzip/codecs/stored_decompressor_test.rb
@@ -9,23 +9,11 @@ require_relative '../test_helper'
 require 'rubyzip/codecs/stored_decompressor'
 
 class StoredDecompressorTest < Minitest::Test
-  # Fake entry which implements compressed_size, name and uncompressed_size.
-  class TestEntry
-    attr_reader :compressed_size, :name, :uncompressed_size
-
-    def initialize(data_size, full_size = nil)
-      @name = 'test_entry'
-      @compressed_size = data_size
-      @uncompressed_size = full_size || data_size
-    end
-  end
-
   def test_read
     text = File.read(TXT_LOREM_IPSUM)
 
     File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(File.size(TXT_LOREM_IPSUM))
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, text.length)
 
       assert_equal(text, decompressor.read)
     end
@@ -36,8 +24,7 @@ class StoredDecompressorTest < Minitest::Test
     half_len = text.length / 2
 
     File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(File.size(TXT_LOREM_IPSUM))
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, text.length)
 
       assert_empty(decompressor.read(0))
       assert_equal(text[(0...half_len)], decompressor.read(half_len))
@@ -47,8 +34,7 @@ class StoredDecompressorTest < Minitest::Test
 
   def test_read_at_eof
     File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(File.size(TXT_LOREM_IPSUM))
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, ::File.size(TXT_LOREM_IPSUM))
       decompressor.read
 
       assert_empty(decompressor.read(0))
@@ -59,49 +45,11 @@ class StoredDecompressorTest < Minitest::Test
 
   def test_short_input
     File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(File.size(TXT_LOREM_IPSUM) + 1)
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
+      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, ::File.size(TXT_LOREM_IPSUM) + 1)
       decompressor.read
 
       assert_raises(EOFError) do
         decompressor.read(1)
-      end
-    end
-  end
-
-  def test_warn_on_long_input
-    text_length = File.size(TXT_LOREM_IPSUM)
-    File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(text_length, text_length - 1)
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
-
-      # Assert that the uncompressed data is detected as too long, but it should
-      # still uncompress the whole thing.
-      assert_output('', /.+'test_entry'.+#{entry.uncompressed_size}B.+/) do
-        buf = decompressor.read
-
-        assert_equal(text_length, buf.length)
-      end
-    end
-  end
-
-  def test_warn_only_once_on_long_input
-    text_length = File.size(TXT_LOREM_IPSUM)
-    half_length = text_length / 2
-    File.open(TXT_LOREM_IPSUM, 'rb') do |is|
-      entry = TestEntry.new(text_length, half_length)
-      decompressor = Rubyzip::Codecs::StoredDecompressor.new(is, entry)
-
-      assert_silent do
-        decompressor.read(100)
-      end
-
-      assert_output('', /.+'test_entry'.+#{entry.uncompressed_size}B.+/) do
-        decompressor.read(half_length)
-      end
-
-      assert_silent do
-        decompressor.read
       end
     end
   end
