f87b3699e2ebf5ec6bb7fb925bbf768180c2b67a
Robert Haines <rhaines@manchester.ac.uk>
2022-09-17 09:13:11 +0100

    Add the Info-ZIP Unix extra field (type 2).

---
+++ lib/rubyzip/extra_fields/unix2.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2022, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative '../utilities'
+require_relative 'field'
+
+##
+module Rubyzip
+  module ExtraFields # :nodoc:
+    # Info-ZIP UNIX Extra Field (type 2).
+    #
+    # This extra field holds 16bit UIDs and GIDs.
+    class Unix2 < Field
+      EXTRA_FIELD_ID = 'Ux'
+
+      attr_reader :gid, :uid
+
+      private
+
+      def merge(data)
+        @uid = Utilities.read16(data)
+        @gid = Utilities.read16(data, 2)
+      end
+    end
+  end
+end
---
+++ test/extra_fields/unix2_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2022, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative '../test_helper'
+
+require 'rubyzip/extra_fields/unix2'
+
+class Unix2Test < Minitest::Test
+  def setup
+    data = "\xE9\x03\xEA\x03"
+    @ux = Rubyzip::ExtraFields::Unix2.new(data)
+  end
+
+  def test_uid
+    assert_equal(1001, @ux.uid)
+  end
+
+  def test_gid
+    assert_equal(1002, @ux.gid)
+  end
+
+  def test_id
+    assert_equal('Ux', @ux.id)
+  end
+
+  def test_label
+    assert_equal('Unix2', Rubyzip::ExtraFields::Unix2.label)
+  end
+end
