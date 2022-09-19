ad955aa47894d0fefad26d34cfadf4510c982f74
Robert Haines <rhaines@manchester.ac.uk>
2022-09-19 14:34:12 +0100

    Wire the configuration into the top-level Rubyzip module.
    And configure tests with default values without pulling the whole
    library into every test.

--- lib/rubyzip.rb
+++ lib/rubyzip.rb
@@ -6,9 +6,16 @@
 
 require_relative 'rubyzip/constants'
 require_relative 'rubyzip/version'
+require_relative 'rubyzip/configuration'
 
 # Rubyzip is a ruby module for reading and writing zip files.
+#
+# Rubyzip is configured here at the top level. See the Configuration
+# module for the details.
 module Rubyzip
+  extend Configuration
+
+  reset!
 end
 
 require_relative 'rubyzip/input_stream'
