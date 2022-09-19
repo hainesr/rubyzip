c1a31eb36796cba439bcb6139ca7a669b47211ec
Robert Haines <rhaines@manchester.ac.uk>
2022-09-19 14:34:12 +0100

    Wire the configuration into the top-level Rubyzip module.
    And configure tests with default values without pulling the whole
    library into every test.

--- test/rubyzip/test_helper.rb
+++ test/rubyzip/test_helper.rb
@@ -9,3 +9,23 @@ require 'minitest/autorun'
 require_relative 'fixtures'
 
 Minitest::Test.make_my_diffs_pretty!
+
+# Configure Rubyzip here without pulling in the entire library.
+#
+# Config fields are set to defaults, and can be overridden using Minitest
+# stubbing, as follows:
+#
+# Rubyzip.stub :error_on_invalid_crc32, false do
+#   <code under test with non-default config>
+# end
+module Rubyzip
+  module_function
+
+  def error_on_invalid_crc32 # rubocop:disable Naming/PredicateMethod
+    true
+  end
+
+  def error_on_invalid_entry_size # rubocop:disable Naming/PredicateMethod
+    true
+  end
+end
