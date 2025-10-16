77eb5c24fe4dd9821a62fa9afdb11269ee8763e8
Robert Haines <rhaines@manchester.ac.uk>
2025-10-16 22:26:10 +0100

    Move current tests to 'zip' sub-directory.

---
+++ test/zip/bzip2_support_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+class Bzip2SupportTest < Minitest::Test
+  BZIP2_ZIP_TEST_FILE = 'test/zip/data/zipWithBzip2Compression.zip'
+
+  def test_read
+    Zip::InputStream.open(BZIP2_ZIP_TEST_FILE) do |zis|
+      error = assert_raises(Zip::CompressionMethodError) do
+        zis.get_next_entry
+      end
+
+      assert_equal(12, error.compression_method)
+      assert_match(/BZIP2/, error.message)
+    end
+  end
+end
