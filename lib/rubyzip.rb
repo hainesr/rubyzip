1644d83f31571acccd084a99e2ecd6c4990c9eb8
Robert Haines <rhaines@manchester.ac.uk>
2025-10-18 08:00:35 +0100

    Add initial Rubyzip library code and test.

---
+++ lib/rubyzip.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+# Rubyzip is a ruby module for reading and writing zip files.
+module Rubyzip
+end
+
+require_relative 'rubyzip/version'
