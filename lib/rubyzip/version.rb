7beac6a7ee717dc780e2ae293d69cd2d4ebe6dab
Robert Haines <rhaines@manchester.ac.uk>
2025-10-18 08:00:35 +0100

    Add initial Rubyzip library code and test.

---
+++ lib/rubyzip/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+##
+module Rubyzip
+  # The version of the Rubyzip library.
+  VERSION = '4.0.0.alpha'
+end
---
+++ test/rubyzip/test_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require 'minitest/autorun'
+
+Minitest::Test.make_my_diffs_pretty!
