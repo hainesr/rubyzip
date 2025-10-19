afc41ea69061cff1daf2da588960f61660b3022e
Robert Haines <rhaines@manchester.ac.uk>
2025-10-19 19:12:08 +0100

    Initialize some `Entry` fields via the header data.

--- test/rubyzip/entry_test.rb
+++ test/rubyzip/entry_test.rb
@@ -9,10 +9,35 @@ require_relative 'test_helper'
 require 'rubyzip/entry'
 
 class EntryTest < Minitest::Test
-  def test_create_with_name
-    name = 'Entry_name.txt'
-    entry = Rubyzip::Entry.new(name)
+  def setup
+    header = File.read(BIN_LOCAL_HEADER)
+    @entry_name = 'lorem_ipsum.txt'
+    @entry = Rubyzip::Entry.new(@entry_name)
+    @entry_with_header = Rubyzip::Entry.new(@entry_name, header)
+  end
 
-    assert_equal(name, entry.name)
+  def test_compressed_size
+    assert_nil(@entry.compressed_size)
+    assert_equal(1439, @entry_with_header.compressed_size)
+  end
+
+  def test_compression_method
+    assert_nil(@entry.compression_method)
+    assert_equal(Rubyzip::COMPRESSION_METHOD_DEFLATE, @entry_with_header.compression_method)
+  end
+
+  def test_crc32
+    assert_nil(@entry.crc32)
+    assert_equal(0xBB66B4EC, @entry_with_header.crc32)
+  end
+
+  def test_name
+    assert_equal(@entry_name, @entry.name)
+    assert_equal(@entry_name, @entry_with_header.name)
+  end
+
+  def test_uncompressed_size
+    assert_nil(@entry.uncompressed_size)
+    assert_equal(3666, @entry_with_header.uncompressed_size)
   end
 end
