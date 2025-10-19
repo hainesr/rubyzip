46b7cb95e6a8f35a0115a0272afc5a5a18011d21
Robert Haines <rhaines@manchester.ac.uk>
2025-10-19 19:40:36 +0100

    Add tests for some GP flags in `Entry`.
    `#encrypted?`, `#streamed?` and `#utf8?`.

--- lib/rubyzip/entry.rb
+++ lib/rubyzip/entry.rb
@@ -15,9 +15,21 @@ module Rubyzip
 
       return unless header
 
-      _sig, _version_to_extract, _fs_type, _gp_flags, @compression_method,
+      _sig, _version_to_extract, _fs_type, @gp_flags, @compression_method,
       _last_mod_time, _last_mod_date, @crc32, @compressed_size, @uncompressed_size,
       _name_len, _extra_len = header.unpack(LOC_PACK)
+    end
+
+    def encrypted?
+      @gp_flags & GP_FLAGS_ENCRYPTED == GP_FLAGS_ENCRYPTED
+    end
+
+    def streamed?
+      @gp_flags & GP_FLAGS_STREAMED == GP_FLAGS_STREAMED
+    end
+
+    def utf8?
+      @gp_flags & GP_FLAGS_UTF8 == GP_FLAGS_UTF8
     end
   end
 end
