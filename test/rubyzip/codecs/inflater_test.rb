33918a88ca836f4b87ec2266dd4facb903fb29a5
Robert Haines <rhaines@manchester.ac.uk>
2025-10-22 21:58:19 +0100

    Clean up the use of `::File` in tests. Not needed.

--- test/rubyzip/codecs/inflater_test.rb
+++ test/rubyzip/codecs/inflater_test.rb
@@ -14,18 +14,18 @@ class InflaterTest < Minitest::Test
     text = File.read(TXT_LOREM_IPSUM)
 
     File.open(BIN_LOREM_IPSUM_DEFLATED, 'rb') do |is|
-      decompressor = Rubyzip::Codecs::Inflater.new(is, ::File.size(BIN_LOREM_IPSUM_DEFLATED))
+      decompressor = Rubyzip::Codecs::Inflater.new(is, File.size(BIN_LOREM_IPSUM_DEFLATED))
 
       assert_equal(text, decompressor.read)
     end
   end
 
   def test_read_read_partial_lengths
-    text = ::File.read(TXT_LOREM_IPSUM)
+    text = File.read(TXT_LOREM_IPSUM)
     half_len = text.length / 2
 
     File.open(BIN_LOREM_IPSUM_DEFLATED, 'rb') do |is|
-      decompressor = Rubyzip::Codecs::Inflater.new(is, ::File.size(BIN_LOREM_IPSUM_DEFLATED))
+      decompressor = Rubyzip::Codecs::Inflater.new(is, File.size(BIN_LOREM_IPSUM_DEFLATED))
 
       assert_empty(decompressor.read(0))
       assert_equal(text[(0...half_len)], decompressor.read(half_len))
@@ -35,7 +35,7 @@ class InflaterTest < Minitest::Test
 
   def test_read_at_eof
     File.open(BIN_LOREM_IPSUM_DEFLATED, 'rb') do |is|
-      decompressor = Rubyzip::Codecs::Inflater.new(is, ::File.size(BIN_LOREM_IPSUM_DEFLATED))
+      decompressor = Rubyzip::Codecs::Inflater.new(is, File.size(BIN_LOREM_IPSUM_DEFLATED))
       decompressor.read
 
       assert_empty(decompressor.read(0))
