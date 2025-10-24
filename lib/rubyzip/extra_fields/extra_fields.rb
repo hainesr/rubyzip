e1918ae8e2e26afa38f37b890dae90c44e87f769
Robert Haines <rhaines@manchester.ac.uk>
2025-10-24 23:28:28 +0100

    Add an extra fields registry for easy lookup.

---
+++ lib/rubyzip/extra_fields/extra_fields.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative 'universal_time'
+
+##
+module Rubyzip
+  module ExtraFields # :nodoc:
+    register_extra_field_type(UniversalTime)
+  end
+end
