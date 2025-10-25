936a0fce142e1a859f26186387c622e880198565
Robert Haines <rhaines@manchester.ac.uk>
2025-10-25 10:25:21 +0100

    Add InfoZip Unix v3 extra field type.

---
+++ lib/rubyzip/extra_fields/unix3.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2002-2025, The Rubyzip Developers.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require_relative '../utilities'
+require_relative 'extra_field'
+
+##
+module Rubyzip
+  module ExtraFields # :nodoc:
+    # Info-ZIP New Unix Extra Field.
+    class Unix3 < ExtraField
+      EXTRA_FIELD_ID = 'ux'
+
+      attr_reader :gid, :uid, :version
+
+      private
+
+      def merge
+        @version = @data.unpack1('C')                                        # offset 0
+        uid_size = @data[1].unpack1('C')                                     # offset 1
+        @uid = @data[2, uid_size].unpack1(Utilities::UNPACK_BYTES[uid_size]) # offset 2
+        gid_size = @data[2 + uid_size].unpack1('C')                          # offset 2 + uid_size
+        @gid = @data[3 + uid_size, gid_size].unpack1(Utilities::UNPACK_BYTES[gid_size])
+      end
+    end
+  end
+end
