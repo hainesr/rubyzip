984fc92fad6b65f4629728a28d8f0641d0d58757
Robert Haines <rhaines@manchester.ac.uk>
2025-10-25 23:16:27 +0100

    Handle different character encodings in Entry names.
    Configurable, but respects the Language Encoding (EFS) GP flag if set.

--- lib/rubyzip/configuration.rb
+++ lib/rubyzip/configuration.rb
@@ -22,12 +22,31 @@ module Rubyzip
   # ```
   module Configuration
     # Configure whether we validate CRC-32 checksums when reading entries.
+    #
     # Default: `true`.
     attr_accessor :error_on_invalid_crc32
 
     # Configure whether we validate extracted size when reading entries.
+    #
     # Default: `true`.
     attr_accessor :error_on_invalid_entry_size
+
+    # Configure which encoding we will use when reading entry names and
+    # comments within Zip archives. According to the Zip standard this
+    # value should only ever need to be `'IBM437'` (IBM Code Page 437) or
+    # `'UTF-8'`. In practice, a lot of Zip tools use whatever character
+    # encoding is set as the default for the OS on which they are running,
+    # which can cause problems when trying to read entry names and comments.
+    # This setting allows Rubyzip to interpret names and comments in any
+    # character encoding that Ruby supports. In the vast majority of cases
+    # this setting should be left alone, but if you know what encoding a Zip
+    # archive has been created with, you can set this to match.
+    #
+    # This setting is overriden if an entry declares that it uses UTF-8
+    # encoding within its header data. In this case UTF-8 is assumed.
+    #
+    # Default: `'UTF-8'`
+    attr_accessor :name_and_comment_encoding
 
     # :call-seq:
     #   reset!
@@ -38,6 +57,7 @@ module Rubyzip
 
       @error_on_invalid_crc32 = true
       @error_on_invalid_entry_size = true
+      @name_and_comment_encoding = 'UTF-8'
     end
 
     # :call-seq:
